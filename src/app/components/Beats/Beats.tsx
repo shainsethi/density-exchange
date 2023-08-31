37bbe3252090af5e8d2d6dc9d1f7dbc0580dc898
shain sethi <shainsethi1000@gmail.com>
2023-09-01 00:04:24 +0530

    completed-beats-component

--- src/app/components/Beats/Beats.tsx
+++ src/app/components/Beats/Beats.tsx
@@ -1,8 +1,36 @@
-import React from "react";
+"use client"
+import React, { useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 const Beats = () => {
+  const { ref , inView } = useInView({
+    threshold: 0.5
+  });
+
+  const left = useAnimation();
+
+  useEffect(()=>{
+    if(inView){
+      left.start({
+        x: 0,
+        transition: {
+          type: 'spring',
+           stiffness: 40
+        }
+      })
+    }
+    if(!inView){
+      left.start({
+        x: '-200vw'
+      })
+    }
+  })
+      
+
   return (
-    <div className="flex justify-center items-start w-3/5 mx-auto gap-5">
+    <motion.div ref={ref}>
+    <motion.div animate={left} className="flex justify-center items-start w-4/5 mx-auto gap-14">
       <div className="Heading w-1/3 text-3xl font-bold">EQ beats IQ</div>
       <div className="Desc text-gray-500 w-1/3 text-base leading-normal font-normal">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
@@ -12,7 +40,8 @@ const Beats = () => {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna 
       </div>
-    </div>
+    </motion.div>
+    </motion.div>
   );
 };
 
